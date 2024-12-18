7d1f7b136604e54ef6ed10d02f44d29fffd8f4b1
Pedro Vinícius <pedrovinic12@gmail.com>
2024-12-18 14:40:19 -0300

    Corrigindo pulo e organizando projeto

--- Assets/scripts/player.cs
+++ Assets/scripts/player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor.Animations;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -13,61 +14,67 @@ public class player : MonoBehaviour
     private AnimatorStateInfo animStateInfo;
     Rigidbody2D rb;
     private string animacao_atual;
-    bool flip = false, jump = false, atacando = false, pro_ataque = false, espada = false, troca_ataque = false;
+    private bool flip = false, jump = false, atacando = false, prox_ataque = false;
+    private bool espada = false, troca_ataque_espada = false;
+    private bool onKeyboard = false;
     private short ataque = 1;
 
-
-    void Start()
-    {
+    void Start(){
         sr = GetComponent<SpriteRenderer>();
         plyer_anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        onKeyboard = true;
     }
 
-    void Update()
-    {
-        if (troca_ataque){
-            if(!espada){
+    void Update(){
+        if (troca_ataque_espada) {
+            if (!espada) {
                 animacao_atual = "SC_espada_anim";
-            } else{
+            }
+            else {
                 animacao_atual = "GD_espada_anim";
             }
-            if(fim_animacao()){
-                troca_ataque = false;
+            if (fim_animacao()) {
+                troca_ataque_espada = false;
                 espada = !espada;
                 Debug.Log("fim");
             }
 
-        } else if (atacando){
+        }
+        else if (atacando){
             atacar();
-        } else{
-            Vector3 pos_passada = transform.position;
-            movimentacao();
-            animar(pos_passada);
+        }
+        else{
+            // Vector3 pos_passada = transform.position;
+            if(onKeyboard)
+                KeyboardControl();
+            animar();
         }
     }
 
-    private void movimentacao(){
+    private void KeyboardControl(){
 
         Vector2 move = new(0, rb.velocity.y);
-        if (Input.GetKeyDown(KeyCode.Space) && !jump){
+        
+        if (Input.GetKeyDown(KeyCode.W) && !jump){
             jump = true;
             move.y = impulso;
         }
-        if (Input.GetKey(KeyCode.RightArrow)){
+        if (Input.GetKey(KeyCode.D)){
             move.x = vel;
         }
-        if (Input.GetKey(KeyCode.LeftArrow)){
+        if (Input.GetKey(KeyCode.A)){
             move.x = -vel;
         }
-        if (Input.GetKeyDown(KeyCode.KeypadEnter) && !atacando){
+        if (Input.GetKeyDown(KeyCode.J) && !atacando){
             atacando = true;
         }
-        if(Input.GetKeyDown(KeyCode.Tab)){
-            troca_ataque = true;
-            if (!espada){
+        if (Input.GetKeyDown(KeyCode.I)){
+            troca_ataque_espada = true;
+            if (!espada) {
                 animacao("SC_espada_anim");
-            } else{
+            }
+            else {
                 animacao("GD_espada_anim");
             }
             Debug.Log(animacao_atual);
@@ -76,25 +83,30 @@ public class player : MonoBehaviour
     }
 
 
-    private void animar(Vector3 pos_passada){
+    private void animar(){
 
         string parado, correndo;
 
-        if(espada){
+        if (espada){
             parado = "P_espada_anim";
             correndo = "C_espada_anim";
-        } else{
+        }
+        else
+        {
             parado = "P_anim";
             correndo = "C_anim";
         }
-        
+
         if (rb.velocity.x > 0){
-            if(flip){
+            if (flip)
+            {
                 flip = false;
                 sr.flipX = false;
             }
-        } else if (rb.velocity.x < 0){
-            if(!flip){
+        }
+        else if (rb.velocity.x < 0){
+            if (!flip)
+            {
                 flip = true;
                 sr.flipX = true;
             }
@@ -103,22 +115,24 @@ public class player : MonoBehaviour
         if (rb.velocity.y > 0){
             animacao("J_anim");
             return;
-        } else if (rb.velocity.y < 0){
+        }
+        else if (rb.velocity.y < 0){
             animacao("Caindo_anim");
             return;
-        } else if (rb.velocity.x != 0){
+        }
+        else if (rb.velocity.x != 0){
             animacao(correndo);
             jump = false;
             return;
         }
         jump = false;
         animacao(parado);
-        
+
     }
 
     private void animacao(string N_animacao){
 
-        if(animacao_atual == N_animacao){
+        if (animacao_atual == N_animacao){
             return;
         }
         animacao_atual = N_animacao;
@@ -129,50 +143,59 @@ public class player : MonoBehaviour
 
         string ataque_1, ataque_2, ataque_3;
 
-        if(espada){
-            ataque_1 = "Ataque_espada_anim";
+        if (espada){
+            ataque_1 = "Ataque_espada_1_anim";
             ataque_2 = "Ataque_espada_2_anim";
             ataque_3 = "Ataque_espada_3_anim";
-        } else{
-            ataque_1 = "Ataque_anim";
+        }
+        else{
+            ataque_1 = "Ataque_1_anim";
             ataque_2 = "Ataque_2_anim";
             ataque_3 = "Ataque_3_anim";
         }
 
-        if(ataque == 1){
+        if (ataque == 1){
             animacao(ataque_1);
-        } else if (ataque == 2){
+        }
+        else if (ataque == 2){
             animacao(ataque_2);
-        } else{
+        }
+        else{
             animacao(ataque_3);
         }
 
-        if (Input.GetKeyDown(KeyCode.KeypadEnter)){
-            pro_ataque = true;
+        if (Input.GetKeyDown(KeyCode.J)){
+            prox_ataque = true;
         }
 
         if (fim_animacao()){
 
-            if (pro_ataque && ataque < 3)
-            {
+            if (prox_ataque && ataque < 3){
                 ataque++;
-                pro_ataque = false;
-            } else{
+                prox_ataque = false;
+            }
+            else {
                 atacando = false;
-                pro_ataque = false;
+                prox_ataque = false;
                 ataque = 1;
             }
         }
     }
 
-    private bool fim_animacao() {
+    private bool fim_animacao(){
         animStateInfo = plyer_anim.GetCurrentAnimatorStateInfo(0);
 
-        if (animStateInfo.IsName(animacao_atual) && animStateInfo.normalizedTime >= 1.0f) {
+        if (animStateInfo.IsName(animacao_atual) && animStateInfo.normalizedTime >= 1.0f){
             return true;
         }
 
         Debug.Log($"Estado atual: {animStateInfo.shortNameHash}, Animação esperada: {animacao_atual}");
         return false;
+    }
+
+    void OnCollisionExit2D(Collision2D collision){
+        if(collision.gameObject.CompareTag("SolidFloor")){
+            jump = true;
+        }
     }
 }
