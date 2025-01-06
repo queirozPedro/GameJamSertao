9194c1562007a2e71a55976f1032f38c50e43b34
DanielNeres <danielnerres@gmail.com>
2025-01-05 22:06:54 -0300

    5.7.1 função para tocar audio

--- Assets/Scripts/game_controler.cs
+++ Assets/Scripts/game_controler.cs
@@ -4,7 +4,18 @@ using UnityEngine;
 
 public class game_controler : MonoBehaviour
 {
-    public void play_audio(string nome_audio){
 
+    public void play_audio(string nome_audio, AudioSource audioSource)
+    {
+        AudioClip clip = Resources.Load<AudioClip>(nome_audio);
+        if (clip != null)
+        {
+            audioSource.clip = clip;
+            audioSource.Play();
+        }
+        else
+        {
+            Debug.LogError("Áudio não encontrado: " + nome_audio);
+        }
     }
 }
