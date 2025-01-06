e75863854b641e4f839be04ea896d1dbb0bd5326
DanielNeres <danielnerres@gmail.com>
2025-01-05 22:10:45 -0300

    V5.7.3 game controler

--- Assets/Scripts/game_controler.cs
+++ Assets/Scripts/game_controler.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class game_controler : MonoBehaviour
 {
+    public AudioSource audioSource;
 
-    public void play_audio(string nome_audio, AudioSource audioSource)
+    public void play_audio(string nome_audio)
     {
         AudioClip clip = Resources.Load<AudioClip>(nome_audio);
         if (clip != null)
